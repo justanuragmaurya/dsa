de105452d56ea8d4cac3dcf08942485715ae8180
justanuragmaurya <justanuragmaurya@gmail.com>
2024-09-11 23:36:07 +0530

    tesst

---
+++ rotated_sorted_array_search.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0 ; i < n ; i++){
+        cin>>arr[i];
+    }
+     int k = 2;
+
+    int l , r ;
+    for(int i = 0 ; i < n ; i ++){
+        
+    }
+
+}
